7670e7bba66001bba90b841c4f3560d892df41aa
CORP\dgiles <dean.giles@transport.nsw.gov.au>
2025-05-06 11:32:30 +1000

    deckoverlay fixes

--- src/components/DeckOverlay.jsx
+++ src/components/DeckOverlay.jsx
@@ -1,28 +1,27 @@
 import useDeckStore from "../store/deckStore";
 
+const DeckOverlay = ({ isOpen }) => {
+  const decks = useDeckStore((state) => state.decks);
+  const deck = decks.find((deck) => deck.id === "default");
+  deck.cards.sort((a, b) => a.cmc - b.cmc);
 
-
-const DeckOverlay = () => {
-
-const decks = useDeckStore((state) => state.decks)
-const deck = decks.find((deck) => deck.id ==='default');
-deck.cards.sort((a, b) => a.cmc - b.cmc);
- 
-
-    return (
-           <div className="bg-black h-screen overflow-auto">
-            <h1 className="flex bg-orange-600 w-1/12 rounded-lg ml-5 mt-2 justify-center">Deck Overlay</h1>
-                <div className="flex flex-wrap justify-start p-4">
-                    {deck.cards.map((card) => (
-                        <img
-                        src={card.image_uris?.border_crop}
-                        alt={card.name}
-                        className="m-2 rounded-lg w-48"
-                        />                    
-                    ))}
-                </div>  
-           </div>
-    )
-}
+  return (
+    <div
+      className={`bg-black overflow-hidden transition-all duration-500 ${
+        isOpen ? "max-h-[10000px]" : "max-h-0"
+      }`}
+    >
+      <div className="flex flex-wrap justify-start p-4">
+        {deck.cards.map((card) => (
+          <img
+            src={card.image_uris?.border_crop}
+            alt={card.name}
+            className="m-2 rounded-lg w-48"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default DeckOverlay;
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,80 +1,98 @@
-import NavBar from "./NavBar"
-import { useState, useEffect  } from "react";
-import Card from "./Card"
+import NavBar from "./NavBar";
+import { useState, useEffect } from "react";
+import Card from "./Card";
 import fetchCards from "../api/mtgApi";
 import Toast from "./Toast";
 import { useLocation } from "react-router-dom";
 import DeckOverlay from "./DeckOverlay";
 
-
 const SearchPage = () => {
-    const location = useLocation();
-    const initialQuery = location.state?.query || "";
-    const [query, setQuery] = useState("");
-    const [cards, setCards] = useState([]);
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [toast , setToast] = useState({message: "", toastStyle: "info"});
-    const [displayDeck, setDisplayDeck] = useState(false);
+  const location = useLocation();
+  const initialQuery = location.state?.query || "";
+  const [query, setQuery] = useState("");
+  const [cards, setCards] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [toast, setToast] = useState({ message: "", toastStyle: "info" });
+  const [displayDeck, setDisplayDeck] = useState(false);
 
-    useEffect(() => {
-        if (initialQuery) {
-            setQuery(initialQuery);
-            handleSearch(initialQuery);
-        }
-    }, [initialQuery]);
-
-    const handleSearch = async (searchTerm = query) => {
-        setError("");
-        setLoading(true);
-        setCards([])
-        try { 
-            const result = await fetchCards(searchTerm);
-            setCards(result);
-        } catch (err) {
-            setError("No cards found");
-        }
-        setLoading(false);
+  useEffect(() => {
+    if (initialQuery) {
+      setQuery(initialQuery);
+      handleSearch(initialQuery);
     }
+  }, [initialQuery]);
 
-    const handleCardError = (message, toastStyle) => {
-        setToast({message, toastStyle});
-      };
-    
-    const clearToast = () => {
-        setToast({message: "", toastStyle: ""});
-      };
-
-    const handleDisplayDeck = () => {
-        if (displayDeck) {
-            setDisplayDeck(false);
-        } else {
-            setDisplayDeck(true);
-        }
+  const handleSearch = async (searchTerm = query) => {
+    setError("");
+    setLoading(true);
+    setCards([]);
+    try {
+      const result = await fetchCards(searchTerm);
+      setCards(result);
+    } catch (err) {
+      setError("No cards found");
     }
+    setLoading(false);
+  };
 
+  const handleCardError = (message, toastStyle) => {
+    setToast({ message, toastStyle });
+  };
 
-    return (
-        <>
-        <NavBar />
-        <div className={`flex flex-col items-center py-5 bg-slate-100 overflow-y-auto tranisition-all duration-500 ${displayDeck ?  "h-[50vh]" : "h-[90vh]"}`}>
-            {loading && <p>Loading...</p>}
-            {error && <p className="text-red-500">{error}</p>}
-            {toast.message && <Toast message={toast.message} clearMessage={clearToast} toastStyle={toast.toastStyle}/>}
-            {cards && (
-                <div className="grid grid-cols-4 gap-4 justify-between mx-24">
-                    {cards.map((card) => (
-                        <Card key={card.id} card={card} cardError={handleCardError}/>
-                    ))}
-                </div>
-                
-            )}
+  const clearToast = () => {
+    setToast({ message: "", toastStyle: "" });
+  };
+
+  const handleDisplayDeck = () => {
+    if (displayDeck) {
+      setDisplayDeck(false);
+    } else {
+      setDisplayDeck(true);
+    }
+  };
+
+  return (
+    <>
+      <NavBar />
+      <div
+        className={`flex flex-col items-center py-5 bg-slate-100 transition-all duration-500 ${
+          displayDeck ? "h-[50vh]" : "h-[93vh]"
+        }`}
+      >
+        {loading && <p>Loading...</p>}
+        {error && <p className="text-red-500">{error}</p>}
+        {toast.message && (
+          <Toast
+            message={toast.message}
+            clearMessage={clearToast}
+            toastStyle={toast.toastStyle}
+          />
+        )}
+        {cards && (
+          <div className="grid grid-cols-4 gap-4 justify-between mx-24">
+            {cards.map((card) => (
+              <Card key={card.id} card={card} cardError={handleCardError} />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="flex flex-row  bg-slate-400">
+        <div className="flex bg-orange-600 w-1/12 rounded-lg ml-5 my-2 justify-center">
+          Deck Overlay
         </div>
-        <div className="flex justify-center text-2xl bg-slate-100 hover:cursor-pointer" onClick={handleDisplayDeck}>{displayDeck ? "v" : "^"}</div>
-        <DeckOverlay />
-        </>
-
-    )
-}
+        <div
+          className={`flex text-2xl absolute left-1/2 text-white hover:cursor-pointer ${
+            displayDeck ? "rotate-180" : ""
+          }`}
+          onClick={handleDisplayDeck}
+        >
+          ^
+        </div>
+      </div>
+      <DeckOverlay isOpen={displayDeck} />
+    </>
+  );
+};
 
 export default SearchPage;
